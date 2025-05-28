222202be961ee816290e4b279ab474c466653529
mohammad javad tavakoli <171261910+mjavadtavakoli@users.noreply.github.com>
2025-05-28 10:25:16 +0330

    Update example.spec.ts

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,18 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('test', async ({ page }) => {
+  await page.goto('http://192.168.1.29:84/#/');
+  await page.getByRole('textbox', { name: 'نام کاربری' }).click();
+  await page.getByRole('textbox', { name: 'نام کاربری' }).fill('your-username');
+  await page.getByRole('textbox', { name: 'نام کاربری' }).press('ControlOrMeta+a');
+  await page.getByRole('textbox', { name: 'نام کاربری' }).press('ControlOrMeta+c');
+  await page.locator('input[name="password"]').click();
+  await page.locator('input[name="password"]').fill('your-pass');
+  await page.getByRole('button').filter({ hasText: /^$/ }).click();
+  await page.getByRole('button', { name: 'ورود' }).click();
 });
